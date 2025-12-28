c6f0d3d228f24268c6dbf2a2755f88fecab4b1a6
Tiana-K08 <tetkononenko2020@gmail.com>
2025-12-28 13:35:12 +0200

    feat: connect Contentful API with app

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import client from '../services/contentful';
+
 import styles from './Page.module.scss';
 
 export default function QuizPage() {
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((response) => console.log(response.items))
+      .catch(console.error);
+  }, []);
+
   return (
     <div className={styles.content}>
       <h1>Quiz ...</h1>
