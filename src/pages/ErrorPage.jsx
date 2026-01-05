2c656ea44027896962f0f3f90b5914b4de519a0f
Tiana-K08 <tetkononenko2020@gmail.com>
2026-01-05 11:48:46 +0200

    style: add styles for Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom';
+
+import styles from './Page.module.scss';
+
 export default function ErrorPage() {
-  return <h1>Щось пішло не так</h1>;
+  return (
+    <div className={styles.errorPage}>
+      <h1>Щось пішло не так... &#128533;</h1>
+      <Link to="/" className={styles.button}>
+        Повернутись на головну
+      </Link>
+    </div>
+  );
 }
