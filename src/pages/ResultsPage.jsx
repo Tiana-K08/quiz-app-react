11654ab842f5b08fff730b969d9837e8228e84dc
Tiana-K08 <tetkononenko2020@gmail.com>
2026-01-05 11:34:23 +0200

    style: add styles for Result Page

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -4,27 +4,18 @@ import styles from './Page.module.scss';
 
 export default function ResultsPage() {
   const answers = useSelector((state) => state.quiz.answers);
-  console.log('All quiz answers:', answers);
-
   const totalQuestions = answers.length;
   const correctAnswersCount = answers.filter((a) => a.isCorrect).length;
   const correctPercentage =
     totalQuestions > 0
       ? Math.round((correctAnswersCount / totalQuestions) * 100)
       : 0;
-  console.log('Total questions:', totalQuestions);
-  console.log('Correct answers:', correctAnswersCount);
-  console.log('Percentage correct:', correctPercentage);
 
-  // return (
-  //   <div className={styles.content}>
-  //     <h1>Results ...</h1>
-  //   </div>
-  // );
+  console.log('All quiz answers:', answers);
 
   return (
-    <div>
-      <h2>Результати вікторини</h2>
+    <div className={styles.content}>
+      <h1>Результати вікторини</h1>
       <p>
         Ви відповіли правильно на {correctAnswersCount} з {totalQuestions}{' '}
         питань.
